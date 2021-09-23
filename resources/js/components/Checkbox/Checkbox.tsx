2d7a1c2b08a0e03391e0e448cfe0391d3b0ae6da
Pascal <pascal.allen88@gmail.com>
2021-09-23 07:56:10 -0500

    update form components

--- resources/js/components/Checkbox/Checkbox.tsx
+++ resources/js/components/Checkbox/Checkbox.tsx
@@ -7,11 +7,13 @@ type Props = {
   className?: string;
   checked?: boolean;
   label?: string;
+  inline?: boolean;
   isValid?: boolean;
   required?: boolean;
   tabIndex?: number;
   disabled?: boolean;
   error?: string;
+  id?: string;
   handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 };
@@ -20,15 +22,16 @@ const Checkbox = (props: Props): React.ReactElement => {
   const {
     formName,
     name,
-    // Optional props
     className = '',
     checked = false,
     label = '',
+    inline = true,
     isValid = true,
     required = false,
     tabIndex = 0,
     disabled = false,
     error = '',
+    id = `${formName}-${name.replace('_', '-')}`,
     handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
       event.preventDefault();
     },
@@ -37,32 +40,32 @@ const Checkbox = (props: Props): React.ReactElement => {
     }
   } = props;
 
-  const id = `${formName}-${name.replace('_', '-')}`;
-
   return (
-    <div className={classnames('form-group form-check', className)}>
-      <input
-        id={id}
-        type="checkbox"
-        name={name}
-        checked={checked}
-        className={classnames('form-check-input', isValid ? '' : 'is-invalid', `${id}-checkbox`)}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        required={required}
-        tabIndex={tabIndex}
-        disabled={disabled}
-      />
-      {label ? (
-        <label htmlFor={id} className={classnames('form-check-label', `${id}-label`)}>
-          {label}
-        </label>
-      ) : null}
-      {error ? (
-        <div id={`${id}-error`} className={classnames('invalid-feedback', `${id}-error`)}>
-          {error}
-        </div>
-      ) : null}
+    <div className={classnames('form-group', className)}>
+      <div className={classnames('form-check', inline ? 'form-check-inline' : '')}>
+        <input
+          id={id}
+          type="checkbox"
+          name={name}
+          checked={checked}
+          className={classnames('form-check-input', !isValid ? 'is-invalid' : '', `${id}-checkbox`)}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          required={required}
+          tabIndex={tabIndex}
+          disabled={disabled}
+        />
+        {label ? (
+          <label htmlFor={id} className={classnames('form-check-label', `${id}-label`)}>
+            {label}
+          </label>
+        ) : null}
+        {error ? (
+          <div id={`${id}-error`} className={classnames('invalid-feedback', `${id}-error`)}>
+            {error}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
